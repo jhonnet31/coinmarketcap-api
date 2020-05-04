5818c0da66541a8a3fbc9d55271781ea11ac0f1b
Jonathan Gonzalez <jhon_gp@outlook.com>
2020-05-03 19:12:38 -0500

    Agregar archivos de proyecto.

---
+++ CRYPTO-API/Controllers/WeatherForecastController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CRYPTO_API.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CRYPTO_API.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly CyptoServicecs srv;
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+
+            srv = new CyptoServicecs();
+        }
+
+        [HttpGet]
+        //[Route("get")]
+        public IActionResult Get()
+        {
+
+            return Ok(srv.getTopCurrencies());
+        }
+
+        [HttpGet]
+        //[Route("prueba")]
+
+        public IActionResult prueba()
+        {
+            return new JsonResult("hola");
+        }
+
+    }
+}
---
+++ CRYPTO-API/Services/CyptoServicecs.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace CRYPTO_API.Services
+{
+    public class CyptoServicecs
+    {
+
+        public object getTopCurrencies()
+        {
+            var URL = new UriBuilder("https://pro-api.coinmarketcap.com/v1/cryptocurrency/quotes/latest");
+
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+            queryString["id"] = "1";
+
+            URL.Query = queryString.ToString();
+
+            var client = new WebClient();
+            client.Headers.Add("X-CMC_PRO_API_KEY", "213dc5e9-0623-4527-81c9-4f46b93546c0");
+            client.Headers.Add("Accepts", "application/json");
+           
+            string data=client.DownloadString(URL.ToString());
+
+            //var json = JObject.Parse(data);
+
+            return data;
+
+        }
+    }
+}
